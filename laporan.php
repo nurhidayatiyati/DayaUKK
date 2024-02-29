29f656cd41b7afbc12653ed70bff13fba49f77e0
dayaukk <nurhidayatiyati761@gmail.com>
2024-02-29 15:28:33 +0700

    first commit

---
+++ laporan.php
@@ -0,0 +1,42 @@
+
+<h1 class="mt-4">Laporan Peminjaman Buku</h1>
+    <div class="card">
+        <div class="card-body">
+        <div class="row">
+            <div class="col-md-12">
+                <a href="cetak.php" target="_blank" class="btn btn-primary"><i class="fa fa-print"></i> Cetak Laporan</a>
+                <br>
+                <br>
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <tr>
+                        <th>No</th>
+                        <th>User</th>
+                        <th>Buku</th>
+                        <th>Tanggal Peminjaman</th>
+                        <th>Tanggal Pengembalian</th> 
+                        <th>Status Peminjaman</th>           
+                    </tr>
+                    <?php
+                    $i = 1;
+                        $query = mysqli_query($koneksi, "SELECT*FROM peminjaman LEFT JOIN user on user.id_user = peminjaman.id_user LEFT JOIN buku on buku.id_buku = peminjaman.id_buku");
+                        while($data =mysqli_fetch_array($query)){
+                            $tgl_peminjam =$data['tanggal_peminjaman'];
+                            $tgl_pengembalian =$data['tanggal_pengembalian'];
+                            $status =$data['status_peminjaman'];
+                            ?>
+                            <tr>
+                            <td><?php echo $i++; ?></td>
+                                <td><?php echo $data['nama']; ?></td>
+                                <td><?php echo $data['judul']; ?></td>
+                                <td><?= date('d-m-Y',strtotime($tgl_peminjam)) ?></td>
+                                <td><?=  date('d-m-Y',strtotime($tgl_pengembalian)) ?></td>
+                                <td><?=$status?></td>
+                            </tr>
+                            <?php
+                        }
+                    ?>
+                </table>
+            </div>  
+        </div>
+    </div>
+ </div>
